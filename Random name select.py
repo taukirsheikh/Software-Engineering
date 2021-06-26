87f98dde14730db0469f870be35ecf9b90cdbff1
Mohammed Taukir Sheikh <54911075+taukirsheikh@users.noreply.github.com>
2021-06-26 08:50:29 +0545

    random name selection created

---
+++ Random name select.py
@@ -0,0 +1,24 @@
+import random
+student = {
+    1: "taukir",
+    2: "aryan",
+    3: "anish",
+    4: "asim",
+    5: "bipana",
+    6: "buddha",
+    7: "tulshi",
+    8: 'dikshya',
+    9: "jaya",
+    10: "monika",
+    11: "nischal",
+    12: "rupesh",
+    13: "kushal",
+    14: "prashant",
+    15: "pratik",
+    17: "ram",
+    18: "rishi",
+    19: "sairam",
+    20: "shreejana"
+}
+selected = random.choice(student)
+print(selected)
